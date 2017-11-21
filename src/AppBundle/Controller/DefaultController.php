acccee05c19e010957d50e3714b3c1497cf9332a
VPopchev <doomham3r@gmail.com>
2017-11-21 21:57:21 +0200

    Added user register/login functionality. user/products functionality almost done.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+use AppBundle\Entity\ProductCategory;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +16,24 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $products = $this
+            ->getDoctrine()
+            ->getRepository(Product::class)
+            ->findAll();
+        foreach ($products as $product){
+            $category = $this->getDoctrine()
+                ->getRepository(ProductCategory::class)
+                ->find($product->getCategoryId());
+            $owner = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($product->getUserId());
+            $product->setCategory($category);
+            $product->setOwner($owner);
+        }
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'products' => $products,
         ]);
     }
 }
